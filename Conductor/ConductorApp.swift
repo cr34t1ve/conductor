e881410e4f65f983718b6512cb63981f1cef233f
Desmond Sofua <mrsofua@gmail.com>
2024-12-19 18:42:50 +0000

    Initial Commit

---
+++ Conductor/ConductorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConductorApp.swift
+//  Conductor
+//
+//  Created by Desmond Sofua on 12/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ConductorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConductorTests/ConductorTests.swift
@@ -0,0 +1,17 @@
+//
+//  ConductorTests.swift
+//  ConductorTests
+//
+//  Created by Desmond Sofua on 12/19/24.
+//
+
+import Testing
+@testable import Conductor
+
+struct ConductorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
